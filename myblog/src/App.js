2091e2d5f5c1aea1f6475840f0ead2e2ab17eb04
Anthony Elorm Zottor <aezottor@gmail.com>
2026-02-18 20:42:53 +0300

    added the react proj

---
+++ myblog/src/App.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import './App.css';
+
+function App() {
+  const [blogs, setBlogs] = useState([])
+  const apiUrl = process.env.REACT_APP_API_URL || '/api'
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then(res => res.json())
+      .then(data => setBlogs(data))
+  }, [apiUrl])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>all blogs</h1>
+        {blogs && blogs.map(blog => (
+          <div key={blog.id}>{blog.title}</div>
+        ))}
+      </header>
+    </div>
+  );
+}
+
+export default App;
